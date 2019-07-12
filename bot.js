afe08bdc93c13be7afa70f2c9a681778ae1785cc
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-07-12 17:33:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,10 +8,142 @@ client.on('ready', () => {
 
 client.on('message', async msg =>  {
 	if(msg.author.bot) return;
-	if (msg.content === '!Launch') {
+	if (msg.content === '!raid ultimus 6') {
 	msg.channel.bulkDelete(100)
     msg.channel.send('```To claim a lane: \n Put an 👌 emoji on the lane of choice.```');
 	msg.channel.send({ files:[('.C:\Users\Jamie\discord-greeter-bot', './images/Ultimus6.png')] });
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 1 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 2 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  	
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 3 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  		
+}
+
+	if (msg.content === '!raid ultimus 7') {
+	msg.channel.bulkDelete(100)
+    msg.channel.send('```To claim a lane: \n Put an 👌 emoji on the lane of choice.```');
+	msg.channel.send({ files:[('.C:\Users\Jamie\discord-greeter-bot', './images/Ultimus7.png')] });
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 1 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 2 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  	
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 3 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  		
+}
+
+	if (msg.content === '!raid alpha 4') {
+	msg.channel.bulkDelete(100)
+    msg.channel.send('```To claim a lane: \n Put an 👌 emoji on the lane of choice.```');
+	msg.channel.send({ files:[('.C:\Users\Jamie\discord-greeter-bot', './images/Alpha4.png')] });
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 1 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 2 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  	
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 3 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  		
+}
+
+	if (msg.content === '!raid Beta 4') {
+	msg.channel.bulkDelete(100)
+    msg.channel.send('```To claim a lane: \n Put an 👌 emoji on the lane of choice.```');
+	msg.channel.send({ files:[('.C:\Users\Jamie\discord-greeter-bot', './images/Beta4.png')] });
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 1 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 2 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  	
+	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 3 ║  \n ╚═══════════════╝ ```")
+    msg.channel.send("Lane 1 - Blue")
+	msg.channel.send("Lane 2 - Yellow") 
+	msg.channel.send("Lane 3 - Red")  
+	msg.channel.send("Lane 4 - Green")  
+	msg.channel.send("Lane 5 - Purple")  
+	msg.channel.send("Lane 6 - White")  
+	msg.channel.send("Lane 7 - Teal")
+	msg.channel.send("Lane 8 - Orange")	  		
+}
+
+	if (msg.content === '!raid gamma 4') {
+	msg.channel.bulkDelete(100)
+    msg.channel.send('```To claim a lane: \n Put an 👌 emoji on the lane of choice.```');
+	msg.channel.send({ files:[('.C:\Users\Jamie\discord-greeter-bot', './images/Gamma4.png')] });
 	msg.channel.send("``` ╔═══════════════╗ \n ║ Strike Team 1 ║  \n ╚═══════════════╝ ```")
     msg.channel.send("Lane 1 - Blue")
 	msg.channel.send("Lane 2 - Yellow") 
