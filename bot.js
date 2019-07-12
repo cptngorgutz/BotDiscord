7448603ed53d8b244a6083813e153335163cde8e
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-07-12 19:37:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ client.on('message', async msg =>  {
 
 	if (msg.content === '!raid alpha 4')	
 	{
-		if (msg.channel.id === 'tb1-alpha') {
+		if (msg.channel.id === '597777446695338005') {
         // Deal with command
 	msg.channel.bulkDelete(100)
     msg.channel.send('```To claim a lane: \n Put an 👌 emoji on the lane of choice.```');
