fe59e46746b29915ce2dffb6e0df2befdca78209
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-09-10 16:37:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -485,7 +485,6 @@ if (msg.content === '!Ultron Basic 1') {
 	}
 	
 	if (msg.content === '!blitz2') {
-	msg.channel.bulkDelete(1)
 	let channel = bot.channels.get("620998744720146432");
 	message.channel.fetchMessages({limit: 1}) 
 	.then(message => console.log(message.content))
