945729976cd85d9e7ec5ccef1e874c832205d26c
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-10-09 17:03:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,10 +37,7 @@ client.on('message', async message =>  {
 	let recruitRole = message.member.guild.roles.find(role => role.name === "recruit2")
 	if (message.content === '!recruit')	{
 	member.addRole(recruitRole);
-	} 
-	catch(errors) {
-    // Code that handles any potential errors
-	}
+	catch(errors) {}} 
 });
 
 	
