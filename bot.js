5bcb055e497b3dc6edc25a1286ab2632eef62c39
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-11-11 22:09:42 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -701,7 +701,7 @@ if (msg.content === '!dafuq3') {
 client.on('messageReactionAdd', (messageReaction, user) => {
 if(user.bot)  return;
 const { message, emoji } = messageReaction;
-if(emoji.name === ":laughing:") {
+if(emoji.name === "😄") {
 if(message.id === "643560144340385793") {
 // code to run when that emoji is reacted on specified message
 	message.channel.send("well done, you smiled.")	  
