69e8b3676a169c59c3b3f26f0752584e90aa4760
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-11-11 02:43:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -691,11 +691,11 @@ if (msg.content === '!commands2') {
 	msg.channel.bulkDelete(1)
 	const exampleEmbed = new Discord.RichEmbed()
 	.setColor('#0099ff')
-	.setAuthor('Ultron', 'https://i.imgur.com/GMx4onc.png', 'https://msf.gg/characters/Ultron')
-	.setDescription('**Disintegration Beam • Level 7**')
+	.setAuthor('The Beyonder', 'https://i.imgur.com/GMx4onc.png', 'https://msf.gg/characters/Ultron')
+	.setDescription('**Commands List**')
 	.attachFiles(['./Misc/UltronBasic.png'])
 	.setThumbnail('attachment://UltronBasic.png')
-	.addField('Attack primary target for 340% damage + steal 2 positive effects + clear Taunt. \nLevel Required: 65', '⠀')
+	.addField('``![characterName]``  Info sheet on chosen character (ex. !ultron) \n``!event [characterName]``  Lowest required team for chosen legendary event (ex. !event starlord) \n``!legendaries``  Requirements for all legendary events \n``!uniques``  List of unique gear needed for all characters 	\n``!blitz``  Current blitz predictions', '⠀')
 	msg.channel.send(exampleEmbed);
 }
 	
