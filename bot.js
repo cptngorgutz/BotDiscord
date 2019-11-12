18ca56ef8baf8cc89aceebfa7b15fa2abe912580
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-11-12 14:42:56 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -717,10 +717,8 @@ message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
 	})
 	.catch(collected => {
 		message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
-})}});
-
- 
-
+});
+}
 
 
 
