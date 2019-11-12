47f06637887216a0665c06534febcd35b27785d7
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-11-12 17:51:31 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('guildMemberAdd', function(member)
 {
 	let memberRole = member.guild.roles.find("name", "recruit");
 	member.addRole(memberRole);
- 	member.guild.channels.get('551566689620197376').send(`Welcome to The Beyonders! ${member}. \nOne of the captains will be with you shortly. \nWe hope you enjoy your stay here. \nPlease post a profile picture in the #profile-images channel. Thankyou.`);
+ //	member.guild.channels.get('551566689620197376').send(`Welcome to The Beyonders! ${member}. \nOne of the captains will be with you shortly. \nWe hope you enjoy your stay here. \nPlease post a profile picture in the #profile-images channel. Thankyou.`);
 	//NEW USERS JOIN
 	member.guild.channels.get('428968153800900608').send({embed: {
 	color: 0x00ff00, 
