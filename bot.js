e88d38698d2e1af5fca67baff3916220ac8a07e2
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-11-12 15:27:27 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -700,12 +700,12 @@ if (msg.content === '!dafuq3') {
 //recruit reacts?
 client.on('message', message => {
 if (message.content === '!roles2') {
-  message.channel.send("Click on :emoji1: to get role1, :emoji2: to get role2 and :emoji3: to get role3.")
   .then(sentMessage => {
     sentMessage.react(":smile:")
     sentMessage.react(":ribbon:")
     sentMessage.react(":laughing:")
   });
+  message.channel.send("Click on :emoji1: to get role1, :emoji2: to get role2 and :emoji3: to get role3.")
 }});
 
 
