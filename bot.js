b7888e8ddce49f098417209ef157b01f965c39e5
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-11-12 13:55:11 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -698,90 +698,113 @@ if (msg.content === '!dafuq3') {
 	
 	
 //recruit reacts?
-client.on('messageReactionAdd', async (reaction, user) => {
-    
-    let applyRole = async () => {
-        let emojiName = reaction.emoji.name;
-        let role = reaction.message.guild.roles.find(role => role.name.toLowerCase() === emojiName.toLowerCase());
-        let member = reaction.message.guild.members.find(member => member.id === user.id);
-        try {
-            if(role && member) {
-                console.log("Role and member found.");
-                await member.roles.add(role);
-                console.log("Done.");
-            }
+
+client.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.content.toLowerCase() === '!createpoll') {
+        if(userCreatedPolls.has(message.author.id)) {
+            message.channel.send("You already have a poll going on right now.");
+            return;
         }
-        catch(err) {
-            console.log(err);
+        message.channel.send("Enter options. Max 5. Type done when finished.");
+        let filter = m => {
+            if(m.author.id === message.author.id) {
+                if(m.content.toLowerCase() === 'done') collector.stop();
+                else return true;
+            }
+            else return false;
+        }
+        let collector = message.channel.createMessageCollector(filter, { maxMatches: 5 });
+        let pollOptions = await getPollOptions(collector);
+        if(pollOptions.length < 2) {
+            message.channel.send("Not enough options, must contain 2!");
+            return;
+        }
+        let embed = new discord.RichEmbed();
+        embed.setTitle("Your Poll");
+        embed.setDescription(pollOptions.join("\n"));
+        let confirm = await message.channel.send(embed);
+        
+        await confirm.react('✅');
+        await confirm.react('❎');
+
+        let reactionFilter = (reaction, user) => (user.id === message.author.id) && !user.bot;
+        let reaction = (await confirm.awaitReactions(reactionFilter, { max: 1 })).first();
+        if(reaction.emoji.name === '✅') {
+            message.channel.send("Poll will begin in 1 seconds.");
+            await delay(1000);
+            message.channel.send("Vote now!");
+            let userVotes = new Map();
+            let pollTally = new discord.Collection(pollOptions.map(o => [o, 0]));
+            let pollFilter = m => !m.bot;
+            let voteCollector = message.channel.createMessageCollector(pollFilter, {
+                time: 60000
+            });
+            userCreatedPolls.set(message.author.id, voteCollector);
+            await processPollResults(voteCollector, pollOptions, userVotes, pollTally);
+            let max = Math.max(...pollTally.array());
+            console.log(pollTally.entries());
+            let entries = [...pollTally.entries()];
+            let winners = [];
+            let embed = new discord.RichEmbed();
+            let desc = '';
+            entries.forEach(entry => entry[1] === max ? winners.push(entry[0]) : null);
+            entries.forEach(entry => desc  += entry[0] + " received " + entry[1] + " votes(s)\n");
+            embed.setDescription(desc);
+
+            if(winners.length === 1) {
+                message.channel.send(winners[0] + " is the winner!", embed);
+            }
+            else {
+                message.channel.send("We have a draw!", embed);
+            }
+        }   
+        else if(reaction.emoji.name === '❎') {
+            message.channel.send("Poll cancelled.");
         }
     }
-    if(reaction.message.partial)
-    {
-        try {
-            let msg = await reaction.message.fetch(); 
-            console.log(msg.id);
-            if(msg.id === '643608256912031775')
-            {
-                console.log("Cached")
-                applyRole();
-            }
+    else if(message.content.toLowerCase() === '!stopvote') {
+        if(userCreatedPolls.has(message.author.id)) {
+            console.log("Trying to stop poll.");
+            userCreatedPolls.get(message.author.id).stop();
+            userCreatedPolls.delete(message.author.id);
         }
-        catch(err) {
-            console.log(err);
-        }
-    }
-    else 
-    {
-        console.log("Not a partial.");
-        if(reaction.message.id === '643608256912031775') {
-            console.log(true);
-            applyRole();
+        else {
+            message.channel.send("You don't have a poll going on right now.");
         }
     }
 });
 
-client.on('messageReactionRemove', async (reaction, user) => {
-    let removeRole = async () => {
-        let emojiName = reaction.emoji.name;
-        let role = reaction.message.guild.roles.find(role => role.name.toLowerCase() === emojiName.toLowerCase());
-        let member = reaction.message.guild.members.find(member => member.id === user.id);
-        try {
-            if(role && member) {
-                console.log("Role and member found.");
-                await member.roles.remove(role);
-                console.log("Done.");
+function processPollResults(voteCollector, pollOptions, userVotes, pollTally) {
+    return new Promise((resolve, reject) => {
+        voteCollector.on('collect', msg => {
+            let option = msg.content.toLowerCase();
+            if(!userVotes.has(msg.author.id) && pollOptions.includes(option)) {
+                userVotes.set(msg.author.id, msg.content);
+                let voteCount = pollTally.get(option);
+                pollTally.set(option, ++voteCount);
             }
-        }
-        catch(err) {
-            console.log(err);
-        }
-    }
-    if(reaction.message.partial)
-    {
-        try {
-            let msg = await reaction.message.fetch(); 
-            console.log(msg.id);
-            if(msg.id === '643608256912031775')
-            {
-                console.log("Cached")
-                removeRole();
-            }
-        }
-        catch(err) {
-            console.log(err);
-        }
-    }
-    else 
-    {
-        console.log("Not a partial.");
-        if(reaction.message.id === '643608256912031775') {
-            console.log(true);
-            removeRole();
-        }
-    }
-})
-	
+        });
+        voteCollector.on('end', collected => {
+            console.log("Collected " + collected.size + " votes.");
+            resolve(collected);
+        })
+    });
+}
 
+function getPollOptions(collector) {
+    return new Promise((resolve, reject) => {
+        collector.on('end', collected => resolve(collected.map(m => m.content.toLowerCase())));
+    });
+}
+
+function delay(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, time)
+    })
+}
 
 
 	
