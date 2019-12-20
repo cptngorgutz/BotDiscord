0e7b844db5c1cfd487a3b38702a4b3cf95b08da6
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-20 02:25:02 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,11 +35,11 @@ client.on('message', (message) => {
 if(message.content.startsWith('!join notifications')){
 message.channel.bulkDelete(1)
 const notify = message.guild.roles.find(r => r.name === "Notifications");
-if(message.member.roles.has(notify.id)) {
-member.removeRole(notify).catch(console.error);
+if(message.author.roles.has(notify.id)) {
+author.removeRole(notify).catch(console.error);
 message.channel.send("" + message.author + "Removed from Notifications")
 } else {
-	member.addRole(notify).catch(console.error);
+	author.addRole(notify).catch(console.error);
 	message.channel.send("" + message.author + "Added to Notifications")
 }
 }});
