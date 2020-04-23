22d4449b6d182d87db4e797aa2e293c9c1d0acff
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-04-23 16:29:11 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1958,7 +1958,7 @@ function function2() {
     if (rows.length) {
 	const exampleEmbed = new Discord.RichEmbed()
 	.setColor('#0099ff')
-	.setDescription(rows.join('\n'))
+	.setDescription(rows.join('\n').replace(/,/g, ' '))
 	message.channel.send(exampleEmbed); 
     } else {
       console.log('No data found.');
